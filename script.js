7ae884d01a5271b096cede61c456071059382591
Katie Ayers <kmildredayers@gmail.com>
2024-07-23 15:00:30 -0600

    updates to navbar

--- script.js
+++ script.js
@@ -20,37 +20,34 @@ function initScroll(elementId){
     window.scroll(0, marginY);
 }
 
-// When the user scrolls the page, execute myFunction
-window.onscroll = function() {myFunction()};
+
+
 // Get the navbar
 var navbar = document.getElementById("navbar");
 // Get the offset position of the navbar
-var sticky = navbar.offsetTop;
+var shouldStickPosition = navbar.offsetTop;
 // Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
-function myFunction() {
-  if (window.scrollY >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
+function addOrRemoveStickyClass() {
+    if (window.scrollY >= shouldStickPosition) {
+        navbar.classList.add("sticky");
+    } else {
+        navbar.classList.remove("sticky");
+    }
 }
 
+window.onscroll = () => {
+    addOrRemoveStickyClass();
+};
 
-// this function is for the scroll to top button 
-let mybutton = document.getElementById("myBtn");
-// When the user scrolls down 20px from the top of the document, show the button
-window.onscroll = function() {scrollFunction()};
+window.onresize = () => {
+    shouldStickPosition = navbar.offsetTop;
+};
 
-function scrollFunction() {
-  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
-    mybutton.style.display = "block";
-  } else {
-    mybutton.style.display = "none";
-  }
-}
+
+
 // When the user clicks on the button, scroll to the top of the document
 function topFunction() {
-//   document.body.scrollTop = 0; // For Safari
+  document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
 }
 
